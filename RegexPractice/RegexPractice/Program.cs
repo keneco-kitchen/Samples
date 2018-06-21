ba0cbd0949350f3e058504801b03f27e9fcec91e
keneco-kitchen <ksk.ktkr.ksk@gmail.com>
2018-06-21 23:36:54 +0900

    正規表現のサンプルを作成した

---
+++ RegexPractice/RegexPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l_reg = new Regex("^[\x20-\x7e]{4,32}$");
+            var l_test = new List<string>()
+            {
+                "abcd",
+                "ab",
+                "This is a pen",
+                "%%%%%",
+                "青い空",
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            };
+            foreach(var l_item in l_test)
+            {
+                if(l_reg.IsMatch(l_item))
+                {
+                    Console.WriteLine($"{l_item} is match.");
+                }
+                else
+                {
+                    Console.WriteLine($"{l_item} is not match.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
